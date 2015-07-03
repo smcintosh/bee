dc3e06127e96ee59c7fec3dab5a8849a1cf4daf2
Cor-Paul Bezemer <bezemer@cs.queensu.ca>
2015-07-03 15:35:48 -0400

    oops bugfix for missing dependency filter

--- lib/bee/report/Neo4jAggregator.rb
+++ lib/bee/report/Neo4jAggregator.rb
@@ -26,24 +26,27 @@ module Bee
      end
      
      def get_rw_nodes()
+       # notice: results of this query represent a bug if there is no dependency n2->n1  !!
        return Neo4j::Session.query("MATCH (node1:gdf)-[:read]->(compiler:strace)-[:write|read*1..10]->(node2:gdf) return node1, node2,compiler")
      end
      
      def find_rw_dependency_bugs()
+       puts "Finding r/w/dependency bugs..."
        # find all dependencies
        get_dependencies()
-
        # find all rw nodes
        rw_nodes = get_rw_nodes()
-       
+       cnt = 0
        # filter nodes in dependencies from rw_nodes
        rw_nodes.each do |n|
          n1 = n.node1[:name]
          n2 = n.node2[:name]               
-         if (!@dependencies.has_key?(n1) || !@dependencies[n1].has_key?(n2))
-           @logger.info("Possible issue found: #{n.node1[:name]} - #{n.node2[:name]} - #{n.compiler[:dir]}/#{n.compiler[:command]}")
-         end
+         if (!@dependencies.has_key?(n2) || !@dependencies[n2].has_key?(n1))
+           @logger.info("Possible issue found: #{n.node1[:name]}-R->#{n.compiler[:dir]}/#{n.compiler[:command]}->#{n.node1[:name]}")           
+         end  
+         cnt = cnt + 1       
        end
+       puts "DONE (total rw_nodes analyzed: #{cnt})"
      end
    end
 end
