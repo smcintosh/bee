144346219446d808d3fa25d3762c7768ebf5c1cc
Cor-Paul Bezemer <bezemer@cs.queensu.ca>
2015-07-29 18:29:13 -0400

    some scripts to make postprocessing easier

---
+++ lib/bee/loader/remove_duplicates.sh
@@ -0,0 +1,6 @@
+# remove duplicate nodes and edges
+head -1 gdf_nodes.csv > nodes_uniq.csv
+tail -n+2 gdf_nodes.csv | sort | uniq -u >> nodes_uniq.csv 
+head -1 gdf_edges.csv > edges_uniq.csv
+tail -n+2 gdf_edges.csv | sort | uniq -u >> edges_uniq.csv 
+
