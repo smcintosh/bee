c52acaf5291f1858029e4084cad8200d2f096f03
Cor-Paul Bezemer <bezemer@cs.queensu.ca>
2015-08-04 09:36:48 -0400

    filter and count patterns in generated issues log

--- lib/bee/report/filter_patterns.sh
+++ lib/bee/report/filter_patterns.sh
@@ -1,20 +1,21 @@
-# script to extract patterns from the possible issues results file
+# script to remove possible issues following certain patterns from the possible issues results file
 #!/bin/bash -xe
 
 ISSUES=$(cat results_qt.log | cut -d ' ' -f 11)
 ISSUES_TOTAL=$(cat results_qt.log | wc -l)
 
 # clear file
-> patterns.log
+> filtered_results_qt.log
 
 # issues have the following format:
 # t3376_opengl_qopengltexturehelper_cpp-R->/home/shane/src/qt/qt-everywhere-opensource-src-5.3.0/qtbase/examples/widgets/draganddrop/puzzle/
 # qtbase/bin/moc->t19379_sub_embedded_make_first
-echo "Starting common pattern analysis"
+echo "Starting to filter results"
 I=0
 for ISSUE in $ISSUES
 do
 	[ "$(($I % 1000))" -eq 0 ] && echo "$I/${ISSUES_TOTAL}"
+	I=$((I+1))
 	TARGETS=(${ISSUE//->/ })
 	# strip -R from the end of the READ_TARGET
 	READ_TARGET="${TARGETS[0]%??}"
@@ -23,12 +24,32 @@ do
 	# get last part of string after _	
 	READ_PATTERN=${READ_TARGET##*_}
 	WRITE_PATTERN=${WRITE_TARGET##*_}
-	PATTERN="${READ_PATTERN}_${WRITE_PATTERN}"
-	echo $PATTERN >> patterns.log
-	I=$((I+1))
-done
-echo "DONE"
+	# this should be a configurable thing but let's hack it for now
+	if [ "${WRITE_PATTERN}" == "pro" ]
+	then 
+		continue
+	fi
 
-echo "Counting pattern occurences"
-sort patterns.log | uniq -c | sort -n -r | tee patterns_count.log
+	if [ "${WRITE_PATTERN}" == "pri" ]
+	then 
+		continue
+	fi
+	if [[ $ISSUE == *"example"* ]]
+	then
+		continue
+	fi
+	
+	if [[ "${WRITE_TARGET}" == *"moc"* ]]
+	then 
+		continue
+	fi
+	
+	if [[ "${WRITE_TARGET}" == *"_first" ]]
+	then 
+		continue
+	fi
+
+	echo $ISSUE >> filtered_results_qt.log
+	
+done
 echo "DONE"
