e4ff60c92b324aedfc2be95788839b0b1f1d3517
bramadams <yasser.araf@belgacom.net>
2016-03-23 21:20:53 -0400

    Adding function to add <root> before each local path.

--- lib/bee/LogUtils.rb
+++ lib/bee/LogUtils.rb
@@ -3,4 +3,15 @@ module LogUtils
     @logger.fatal(msg)
     raise msg
   end
+
+  #replace "prefix" in "thefname" by "<root>"
+  def rootify(thefname,prefix)
+    
+    if (thefname.start_with?(prefix))
+      thefname=thefname.sub(prefix,"<root>")
+    end
+    
+    return thefname
+  end
+  
 end
